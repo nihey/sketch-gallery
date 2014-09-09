29b97b5afff04b25a32c6f93f839468b5bb3a187
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-09-08 21:35:27 -0300

    Index showing message to start drawing.

--- sketch-frontend/app/controllers/index.js
+++ sketch-frontend/app/controllers/index.js
@@ -4,6 +4,19 @@ export default Ember.Controller.extend({
 
   actions: {
 
+    startArtwork: function() {
+      this.set('formError', false);
+      var sketches = this.get('sketches');
+      var artwork = this.get('artwork');
+      for (var i = 0; i < sketches.length; i++) {
+        if (artwork === sketches[i].url) {
+          this.set('formError', true);
+          return;
+        }
+      }
+      this.transitionToRoute('/' + artwork);
+    },
+
     setupSketches: function() {
       var sketches = this.get('sketches');
       Ember.$(sketches).each(function (index, sketch) {
