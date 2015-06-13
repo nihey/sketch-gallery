281e58d145fec0c81287bd7f799ef155f0c8c722
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-06-13 18:31:54 -0300

    Add firebase dependency

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'sketch',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://sketch-gallery.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
